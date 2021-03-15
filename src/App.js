db01a38c9c958b5a394200a344769bfae97a3be3
hvtk <hvtk1968@gmail.com>
2021-03-15 10:35:23 +0100

    first draft without controlled components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,40 @@ import './App.css';
 function App() {
   return (
     <div>
-      Maak hier jouw formulier!
+        <form>
+        <fieldset>
+        <legend> Gegevens </legend>
+            <label for="naam"> Naam: </label>
+            <input
+            type="text"
+            />
+            <br/>
+            <label for="leefijd"> Leeftijd: </label>
+            <input
+            type = "text"
+            placeholder="0"
+            />
+        </fieldset>
+        </form>
+        <form>
+        <fieldset>
+        <legend> Jouw review </legend>
+            <label for="hoe heb je dit recept gevonden?"> Hoe heb je dit recept gevonden? <br/> </label>
+            <select name="hoe heb je dit recept gevonden?" id="hoe heb je dit recept gevonden?">
+            <option value="Anders"> Anders </option>
+            <option value="Google"> Google </option>
+            <option value="Vriend"> Vriend </option>
+            <option value="Advertentie"> Advertentie </option>
+            </select>
+            <br/>
+            <label for="opmerkingen"> Opmerkingen: <br/> </label>
+            <textarea name="opmerkingen" id="opmerkingen">
+                wat vond je van het recept?
+            </textarea>
+            <br/>
+            <button type="submit"> Versturen </button>
+        </fieldset>
+        </form>
     </div>
   );
 }
